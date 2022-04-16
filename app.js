52615e8ee430f7627dbf310757b302250100779e
anducnguyen <lordmasterking001@gmail.com>
2022-04-16 20:44:49 +0700

    init project

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
-import Express  from "express";
+import express from "express";
+import mongoose from "mongoose";
+import morgan from "morgan";
+import cors from "cors";
+import 'dotenv/config'
 
-const app = Express();
+const app = express();
+
+app.use(express.json({ limit: '5mb', extended: true }));
+app.use(express.urlencoded({ limit: '5mb', extended: true }));
+app.use(cors());
+app.use(morgan("dev"));
+
+mongoose.connect(process.env.DB, { useNewUrlParser: true, useUnifiedTopology: true }, (error) => {
+    if (error) {
+        throw error;
+    }
+    console.log("MongoDb connected!");
+});
 
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+app.listen(process.env.PORT || 5000, () => {
+    console.log(`Server started on port ${process.env.PORT}`);
 })
