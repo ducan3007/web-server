18907ad0e09a4c9b306e63daeb17d6700cb3b191
Nguyen Duc An <58422724+ducan3007@users.noreply.github.com>
2022-05-04 21:55:18 +0700

    Update plan.route.js

--- routes/plan.route.js
+++ routes/plan.route.js
@@ -1,10 +1,10 @@
 import express from "express";
 
-const plan_route = express.Router();
+const plan_routes = express.Router();
+
+
+plan_routes.route("/plan/:id").get(get_plan);
 
 
 
-
-
-
-export default plan_route;
+export default plan_routes;
